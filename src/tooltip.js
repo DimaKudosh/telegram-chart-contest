dd2bd929b1524edee0135b2945ef7feeb319fa74
Dima Kudosh <LagMegantic@yandex.ru>
2019-03-23 21:49:45 +0300

    Fixed tooltip overflow, optimzied tooltip clear

--- src/tooltip.js
+++ src/tooltip.js
@@ -9,12 +9,12 @@ export default class Tooltip extends BaseUIElement {
         this.datasets = chart.datasets;
         this.maxValue = chart.maxValue;
         this.chart = chart;
-        this.cache = {};
+        this.offsetX = 25;
+        this.lastIndex = 0;
 
+        this.create();
         canvas.canvas.addEventListener('mousemove', this.onMouseMove.bind(this), true);
         canvas.canvas.addEventListener('mouseout', this.onMouseOut.bind(this), true);
-        [this.mainContainer, this.title, this.containers, this.values] = this.create();
-        this.offsetX = 25;
     }
 
     updateOptions(options) {
@@ -53,16 +53,32 @@ export default class Tooltip extends BaseUIElement {
         div.appendChild(title);
         div.appendChild(allDatasetsContainers);
         this.chart.container.appendChild(div);
-        return [div, title, datasetContainers, datasetValues];
+        this.mainContainer = div;
+        this.title = title;
+        this.containers = datasetContainers;
+        this.values = datasetValues;
     }
 
-    renderLabel(label) {
-        let val = this.cache[label];
-        if (!val) {
-            val = tooltipTimestampToString(label);
-            this.cache[label] = val;
+    placeTooltip(x) {
+        const width = this.canvas.width;
+        const container = this.mainContainer;
+        const containerWidth = container.clientWidth;
+        const preferredPosition = x - this.offsetX;
+        let position;
+        if (preferredPosition < 5) {
+            position = 5;
+        } else if (preferredPosition + containerWidth > width - 5) {
+            position = width - containerWidth - 5;
+        } else {
+            position = preferredPosition;
         }
-        return val;
+        container.style.left = position + 'px';
+    }
+
+    clear() {
+        const {canvas, ctx} = this;
+        const [x, h] = canvas.translatePoint(this.lastIndex, 0);
+        ctx.clearRect(x - 15, 0, 30, h);
     }
 
     onMouseMove(e) {
@@ -71,8 +87,8 @@ export default class Tooltip extends BaseUIElement {
         this.mainContainer.style.backgroundColor = this.options.backgroundColor;
         const x = e.offsetX - canvas.canvas.offsetLeft;
         const index = Math.floor((x / canvas.width) * labels.length);
-        this.title.textContent = this.renderLabel(labels[index]);
-        canvas.clear();
+        this.title.textContent = tooltipTimestampToString(labels[index]);
+        this.clear();
         canvas.drawLine([[index, 0], [index, this.maxValue]], options.color, 2);
         for (let i = 0; i < datasets.length; i++) {
             const dataset = datasets[i];
@@ -86,11 +102,12 @@ export default class Tooltip extends BaseUIElement {
             this.values[i].textContent = dataset.data[index];
             this.canvas.drawArc(index, dataset.data[index], 4, dataset.color, 4, options.backgroundColor);
         }
-        this.mainContainer.style.left = (x - this.offsetX) + 'px';
+        this.placeTooltip(x);
+        this.lastIndex = index;
     }
 
     onMouseOut() {
-        this.canvas.clear();
+        this.clear();
         this.mainContainer.style.display = 'none';
     }
 }
